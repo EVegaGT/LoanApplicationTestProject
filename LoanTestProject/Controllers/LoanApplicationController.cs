d197f743d9bd1d28cf399313d24910307384e15e
Edgar Vega <edgarvega54@gmail.com>
2026-08-12 20:59:54 -0600

    Add project files.

---
+++ LoanTestProject/Controllers/LoanApplicationController.cs
@@ -0,0 +1,19 @@
+﻿using LoanTestProject.Payload;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoanTestProject.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoanApplicationController : ControllerBase
+    {
+        // POST: api/LoanApplication
+        [HttpPost]
+        public IActionResult Post([FromBody] LoanApplication application)
+        {
+            //This just an Mock external service, We will return Ok always
+            return Ok();
+        }
+    }
+}
---
+++ LoanTestProject/Payload/LoanApplication.cs
@@ -0,0 +1,12 @@
+﻿namespace LoanTestProject.Payload
+{
+    public class LoanApplication
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string State { get; set; }
+        public decimal Amount { get; set; }
+        public string Ssn { get; set; }
+    }
+}
